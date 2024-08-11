efb865eb6350248619808c5c08b9803c2915723d
Lorenzo Nolasco <Lorenzonolasco23@gmail.com>
2024-08-11 06:50:06 -0600

    deleted host and port

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,6 @@ if (process.env.DB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    process.env.DB_HOST,
-    process.env.DB_PORT,
     {
       host: 'localhost',
       dialect: 'postgres',
